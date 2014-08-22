060ee4fcad27d008a692cc9c43345d4e69d9609e
munseonju <msj4583@gmail.com>
2014-08-22 10:03:24 +0900

    .

---
+++ personal_space/seonju/examplespace/ngbot/app003/js/carousel.js
@@ -0,0 +1,22 @@
+/**
+*  Module
+*
+* Description
+*/
+angular.module('ngbot.carousel', ['ui.bootstrap'])
+.controller('homeCarouselCtrl', ['$scope', function($scope){
+		$scope.myInterval = 2000;
+  var slides = $scope.slides = [];
+  var imgname = -1;
+  $scope.addSlide = function() {
+    slides.push({
+      // image: 'http://lorempixel.com/500/500',
+      image: 'http://lorempixel.com/' + parseInt(Math.random()*1000) + '/' + parseInt(Math.random()*1000),
+      text: ['More','Extra','Lots of','Surplus'][slides.length % 4] + ' ' +
+        ['Cats', 'Kittys', 'Felines', 'Cutes'][slides.length % 4]
+    });
+  };
+  for (var i=0; i<3; i++) {
+    $scope.addSlide();
+  }
+}])
