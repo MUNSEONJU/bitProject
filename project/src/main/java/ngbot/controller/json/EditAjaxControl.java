216a21dc4a278dee0ca34fa3182399f0454402ad
joseonguk <tjddnrdl0@gmail.com>
2014-09-19 15:27:56 +0900

    project

---
+++ project/src/main/java/ngbot/controller/json/EditAjaxControl.java
@@ -0,0 +1,50 @@
+//찾는부분
+package ngbot.controller.json;
+
+import java.util.HashMap;
+
+import ngbot.service.NgbotService;
+import ngbot.vo.Ngbot;
+
+import org.apache.log4j.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+@RequestMapping("/edit")
+public class EditAjaxControl {
+  static Logger logger = Logger.getLogger(EditAjaxControl.class);
+  
+  @Autowired
+  NgbotService ngbotService;
+  
+  @RequestMapping(value="{str}", method=RequestMethod.POST) 
+  public Object view(@PathVariable String str) throws Exception {
+    logger.warn(str+" 목록 가져오기.....");
+    HashMap<String,Object> result = new HashMap<String,Object>();
+    
+    logger.warn(str);
+    
+    Ngbot ngbot = ngbotService.sel(str);
+    
+    if(ngbot == null) {
+      result.put("status", "success");
+      
+      logger.warn("db에 값이 없음");
+      
+      return result;
+    }
+    else {
+      result.put("config", ngbot.getConfig());
+      result.put("html", ngbot.getHtml());
+      result.put("js", ngbot.getJs());
+      
+      logger.warn("Dao에서 "+str+" 가져오기");
+      
+      return result;
+    }
+  }
+}
---
+++ project/src/main/java/ngbot/vo/Ngbot.java
@@ -0,0 +1,41 @@
+package ngbot.vo;
+
+public class Ngbot {
+  private int no;
+  private String randstr;
+  private String config;
+  private String html;
+  private String js;
+  
+  public String getConfig() {
+    return config;
+  }
+  public void setConfig(String config) {
+    this.config = config;
+  }
+  public String getRandstr() {
+    return randstr;
+  }
+  public void setRandstr(String randstr) {
+    this.randstr = randstr;
+  }
+  public int getNo() {
+    return no;
+  }
+  public void setNo(int no) {
+    this.no = no;
+  }
+  public String getHtml() {
+    return html;
+  }
+  public void setHtml(String html) {
+    this.html = html;
+  }
+  public String getJs() {
+    return js;
+  }
+  public void setJs(String js) {
+    this.js = js;
+  }
+  
+}
