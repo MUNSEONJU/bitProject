aa9e0fd1c67f973ae36fd0e332ef3e099f6e31f3
munseonju <msj4583@gmail.com>
2014-09-20 18:19:02 +0900

    .

---
+++ Hello/gen/java56/hello/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package java56.hello;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ Hello/src/java56/hello/MyCalc.java
@@ -0,0 +1,11 @@
+package java56.hello;
+
+//import android.webkit.JavascriptInterface
+
+public class MyCalc {
+	
+	//@JavascriptInterface 4.4.2ÇØ¾ßµÇ..¾ø¾û...
+	public int plus(int a, int b){
+		return a+b;
+	}
+}
