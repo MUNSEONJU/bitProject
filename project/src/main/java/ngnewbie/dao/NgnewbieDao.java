7961b332b1a6a3cf1f37d4edc9116489a1c74305
joseonguk <tjddnrdl0@gmail.com>
2014-09-25 14:37:09 +0900

    project
    .

--- project/src/main/java/ngnewbie/dao/NgnewbieDao.java
+++
@@ -1,8 +0,0 @@
-package ngnewbie.dao;
-
-import ngnewbie.vo.Ngnewbie;
-
-public interface NgnewbieDao {
-  int insert(Ngnewbie ngbot) throws Exception;
-  Ngnewbie sel(String str) throws Exception;
-}
--- project/src/main/java/ngnewbie/service/NgnewbieService.java
+++
@@ -1,9 +0,0 @@
-package ngnewbie.service;
-
-import ngnewbie.vo.Ngnewbie;
-
-public interface NgnewbieService {
-  //Ngbot insert(Ngbot ngbot) throws Exception;
-  void insert(Ngnewbie ngbot) throws Exception;
-  Ngnewbie sel(String str) throws Exception;
-}
