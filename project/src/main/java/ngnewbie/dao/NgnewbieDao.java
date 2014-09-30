cf43e41ce51e60123f511b3ead81561c6d200adc
joseonguk <tjddnrdl0@gmail.com>
2014-09-30 16:15:59 +0900

    project
    .

--- project/src/main/java/ngnewbie/dao/NgnewbieDao.java
+++
@@ -1,8 +0,0 @@
-package ngnewbie.dao;
-
-import ngnewbie.vo.Ngnewbie;
-
-public interface NgnewbieDao {
-  int insert(Ngnewbie ngbot) throws Exception;
-  Ngnewbie select(String urlhash) throws Exception;
-}
