777da598890b3ff802032a98a270097dfefea013
munseonju <msj4583@gmail.com>
2014-10-07 17:03:47 +0900

    .

--- ngNewbie/js/app.js
+++ ngNewbie/js/app.js
@@ -1,36 +1,106 @@
+
 var app = angular.module('application', ['ngRoute','ngNewbieTools']);
 
-app.config( function($routeProvider){
-		$routeProvider
-			.when('/main',{templateUrl: 'partials/main.html'})
-			.when('/aa',{templateUrl: 'partials/temp/aa.html'})
-			.when('/bb',{templateUrl: 'partials/temp/bb.html'})
-			.when('/cc',{templateUrl: 'partials/temp/cc.html'})
-			.otherwise({redirectTo: '/main'})
-});
+app.controller('commonController', function($location,$window,$scope,$newbie){
+	var checkStat = 'active';
+	var inactiveOffset = $("#topnav").offset().top;
+	//topnav의 항목 리스트
+	$scope.navList = ['aa', 'bb', 'cc'];
+	
+	$scope.inactive = function(doClick){
+		if(checkStat != 'inactive'){
+			checkStat = 'inactive';
+			$("#header").css("display","none");
+			
+			$("#topnav").addClass("inactive");
+			$("#topnav>.navi").addClass("inactive");
+			$("#topnav>.logo-navi").addClass("inactive");
+			$("#content-wrap").addClass("inactive");
+			if(doClick == null){
+				window.scrollTo(0,100);	
+			}; 
 
-app.controller('commonController', function($scope){
+			setTimeout(function(){
+				$("#header").addClass("inactive");
+			},100);		
+		}
+	}
+	$scope.active = function(){
+		checkStat = 'active';
+		$("#header").css("display","block");
+		setTimeout(function(){
+			$("#header").removeClass("inactive");
+			$("#topnav").removeClass("inactive");
+			$("#topnav>.navi").removeClass("inactive");
+			$("#topnav>.logo-navi").removeClass("inactive");
+			$("#content-wrap").removeClass("inactive");
+		},100);
+	}
+	
+	$scope.inactiveClicked = function(scroll){
+		checkStat = 'inactive';
+		scroll();
+		$("#header").addClass("inactive");
+		setTimeout(function(){
+			$("#header").css("display","none");
 
-		$(".remocon").children("div.toggleMenu").click(function(){
-
-			if( $("#menu-wrap").hasClass("opened") ){
-			  $("#contents").removeClass("opened");
-			  $("#menu-wrap").removeClass("opened");
-			  $("#transparency-cover").removeClass("opened");
-			  $("#topbar").removeClass("opened");
-			} else {
-			  $("#contents").addClass("opened");
-			  $("#menu-wrap").addClass("opened");
-			  $("#transparency-cover").addClass("opened");
-			  $("#topbar").addClass("opened");
+			$("#content-wrap").addClass("inactive");
+			$("#topnav").addClass("inactive");
+			$("#topnav>.navi").addClass("inactive");
+			$("#topnav>.logo-navi").addClass("inactive");
+		},100);	
+	}
+	
+	$scope.navClick = function(n){
+		
+		//클릭한 버튼이 첫번째가 아닐때 스크롤 위치를 제어한다.
+		if(n == $scope.navList[0] && checkStat == 'active'){
+			$scope.inactiveClicked(	function(){ window.scrollTo(0,200) })
+		
+		} else if(checkStat == "active"){
+//			$($window).unbind('scroll');
+			$scope.inactiveClicked( function(){ 
+				$("html, body").animate(
+					{ scrollTop: $('.content-' + n).offset().top - 280 },300);		
+			});
+			
+//			$($window).bind('scroll',function(){
+//				if($newbie.scrollLocator("Y").currLoc > 440){
+//					$scope.inactive();
+//				}
+//				if($("#header").css("display")=="none"){
+//					if($newbie.scrollLocator("Y").currLoc == 0){
+//						$scope.active();
+//					}
+//				}
+//			});
+			
+		} else{
+			$scope.inactiveClicked( function(){ 
+				$("html, body").animate(
+					{ scrollTop: $('.content-' + n).offset().top  - 20 },300);		
+			}); 
+		}
+		
+	}
+	
+	$scope.logoClick = function(){
+		$("html, body").animate(
+				{ scrollTop: 0 },500);
+	}
+	
+	window.scrollTo(0,0);
+	$($window).bind('scroll', function(){
+		
+		if($newbie.scrollLocator("Y").currLoc > 440){
+			$scope.inactive(100);
+		}
+		
+		if($("#header").css("display")=="none"){
+			if($newbie.scrollLocator("Y").currLoc == 0){
+				$scope.active();
 			}
-		})
-		$(".remocon").children("div.toggleNav").click(function(){
-			if( $("#header").hasClass("inactive") ){
-			  $("#header").removeClass("inactive");
-			} else{
-			  $("#header").addClass("inactive");
-			}
-		})
+		}
+}	);
 
 });
--- ngNewbie/js/tools.js
+++ ngNewbie/js/tools.js
@@ -5,6 +5,7 @@ angular.module('ngNewbieTools', [])
       currLoc: 0,
       preLoc:0,
 
+      
       simpleToggle: function(selector){
         if( $(selector).css("display") == "block" ){
           $(selector).css("display","none");
@@ -14,7 +15,7 @@ angular.module('ngNewbieTools', [])
 
       /**
       * scrollHider
-      *
+      * + 스크롤 내리면 사라지고 올리면 나타나는.. 연습용이었음...
       * + paramenters
       *   -selector : 숨길거(선택자)
       *   -doNotHide : 숨기지 않을 범위(좌표 2차원배열)
@@ -32,8 +33,8 @@ angular.module('ngNewbieTools', [])
       	if(this.currLoc>this.preLoc){
       		$(selector).css("display","none");
       	} else 
-	   			// (나머지)지정한 범위가 아닌경우 - 스크롤 아래로 : block
-	   			$(selector).css("display","block");
+			// (나머지)지정한 범위가 아닌경우 - 스크롤 아래로 : block
+			$(selector).css("display","block");
 
       	for(var i=0 ; i<this.sectionArr.length ; i++){
 	      	// 지정 범위 안에 속할 경우 : block
@@ -49,7 +50,26 @@ angular.module('ngNewbieTools', [])
       	} else {
       		$(selector+" *").css("color","black");
       	}
-     	}
+      }
+      
+      /**
+       * classAdder,classRemover
+       * 
+       * + parameters
+       * 	- classname : 지우거나 추가할 클래스 이름
+       * 	- selectorArr : 지우거나 추가될 선택자들의 배열
+       */
+      ,classAdder:function(classname,selectorArr){
+		  for(var i=0;i<selectorArr.length;i++){
+			$(selectorArr[i]).addClass(classname);
+		  }
+      }
+      
+      ,classRemover:function(classname,selectorArr){
+    	  for(var i=0;i<selectorArr.length;i++){
+			$(selectorArr[i]).removeClass(classname);
+    	  }
+      }
       
       /**
       * toggler
@@ -58,17 +78,50 @@ angular.module('ngNewbieTools', [])
       *   -before: element{ ... }
       *   -after : element.[유저가 지정한 클래스이름]{ ... }
       * + parameters
-      *   -selector : toggle 대상 선택자
+      *   -standardSelector : 기준 element
       *   -classname : after 지정 클래스명
+      *   -selectorArr : toggle 대상 선택자 배열
       */
-      ,toggler:function(selector,classname){
-        var element = $(selector);
-          if( element.hasClass(classname) ){
-            element.removeClass(classname);
-          } else {
-            element.addClass(classname);
-          }
+      ,toggler:function(standardSelector,classname,selectorArr){
+		  if( $(standardSelector).hasClass(classname) ){
+			this.classRemover(classname,selectorArr);
+		  } else {
+		    this.classAdder(classname,selectorArr);
+		  }
+    	  
+       }
+      
+      /**
+       * scrollLocator
+       * 
+       * + parameters
+       * 	- direction : 방향. x 또는 y 입력, 입력안하면 y, 기타입력 예외출력
+       * + return object - location
+       * 	:$newbie.scrollLocator().preLoc / $newbie.scrollLocator().currLoc
+       *   	:또는 scrollLocator함수 수행 후 $newbie서비스의 preLoc/currLoc으로 접근
+       */
+      ,scrollLocator:function(direction){
+    	  var preLoc = '';
+    	  var currLoc = '';
+    	  var location = {};
+    	  if( direction==null || direction=="y" || direction=="Y" ){
+    		  this.preLoc = this.currLoc;
+    	      this.currLoc = window.pageYOffset;
+    	  } else if( direction=="x" || direction=="X" ){
+    		  this.preLoc = this.currLoc;
+    	      this.currLoc = window.pageXOffset;
+    	  } else{
+    		  console.log("$newbie.scrollLocator - direction exception");
+    		  this.preLoc = null;
+    	      this.currLoc = null;
+    	  }
+	      preLoc = this.preLoc;
+	      currLoc = this.currLoc;
+	      location = {"preLoc": preLoc, "currLoc": currLoc};
+	      
+    	  return location;
       }
+      
 
 
     }
